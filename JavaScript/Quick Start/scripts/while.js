65d2d9e901784ddbc7ade66815df09b5ae66f93a
sharma7n <sharma7n@gmail.com>
2017-04-28 18:07:38 +0000

    cardio donuts

---
+++ JavaScript/Quick Start/scripts/while.js
@@ -0,0 +1,11 @@
+var i = 0;
+do {
+    console.log(i);
+    i++;
+} while ( i < 10 );
+
+var j = 10
+while ( j > 0 ) {
+    console.log(j);
+    j--;
+};
---
+++ JavaScript/Quick Start/scripts/wordchain.js
@@ -0,0 +1,33 @@
+function* letters( str ) {
+    for ( var i = 0; i < str.length; i++ ) {
+        yield str[i];
+    }
+}
+
+function* ienumerate ( iter ) {
+    var item = null;
+    var done = false;
+    var index = 0;
+    while( !done ) {
+        item = iter.next();
+        yield {
+            index: index,
+            value: item.value,
+        };
+        index++;
+        done = item.done;
+    };
+}
+
+function* chain ( initial, final ) {
+    var intermediate = initial;
+    for ( var pack of ienumerate(letters(final)) ) {
+        var index = pack.index;
+        var letter = pack.value;
+        yield intermediate;
+        intermediate = intermediate.substr(0, index) + letter + intermediate.substr(index + 1);
+    }
+}
+
+var result = Array.from(chain("cardio", "donuts"));
+alert(result);
